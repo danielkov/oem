0d025f8d19ea546eed170c1d8ae6db0ef0bee262
Daniel Emod Kovacs <kovacsemod@gmail.com>
2019-11-24 14:29:35 +0100

    refactor(cli): better logging

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -8,23 +8,31 @@ const stripDash = (argument: string) => argument.replace(/^-?-/, "");
 const isArgumentName = (candidate: string) => /^(-\w$)|(--\w+)/.test(candidate);
 
 const parse = (args: string[]) => {
+  log`Parsing arguments: ${args}`;
   const parsed: any = {};
   for (let index = 0; index < args.length; index++) {
     let argument = args[index];
+    log`Parsing argument: ${argument}`;
     if (!isArgumentName(argument)) {
+      log`${argument} is not a valid name for an argument - bailing`;
       throw new Error(`Failed to parse argument: ${argument} - invalid name`);
     }
     let value;
+    log`Attempting to parse name=value notation`;
     const split = argument.split("=");
     if (split[1]) {
+      log`We have a name=value notation`;
       value = split[1];
       argument = split[0];
     } else if (args[index + 1] && !isArgumentName(args[index + 1])) {
+      log`argument value is in the next element - skipping it`;
       value = args[index + 1];
       index += 1;
     } else {
+      log`No value associated with argument - treating it as boolean`;
       value = true;
     }
+    log`Argument name is ${argument} and value is ${value}`;
     parsed[stripDash(argument)] = value;
   }
   return parsed;
@@ -48,6 +56,8 @@ const cli = async (config: OemConfig) => {
   const command = process.argv[2];
 
   const args = parse(process.argv.slice(3));
+
+  log`After parsing we have command: ${command} with arguments: ${args}`;
 
   return { command, args };
 };
